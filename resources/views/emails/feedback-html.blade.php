ab9c215bad2b6ee07840ada4cba37f8b6fb70692
George <george@internetguru.io>
2025-09-24 08:07:52 +0200

    Update email templates

--- resources/views/emails/feedback-html.blade.php
+++ resources/views/emails/feedback-html.blade.php
@@ -1,24 +1,18 @@
 @extends('ig-common::layouts.email-html')
 
 @section('content')
-<p>@lang('ig-common::messages.email.hello')</p>
-
 <p>
-    <strong>@lang('ig-feedback::layouts.form.message')</strong><br>
+    <strong>@lang('ig-feedback::layouts.form.message'):</strong><br>
     {{ $feedback['message'] ?? __('ig-feedback::layouts.email.no_message') }}
 </p>
-
 <p>
-    <strong>@lang('ig-feedback::layouts.form.email')</strong><br>
+    <strong>@lang('ig-feedback::layouts.form.email'):</strong><br>
     {{ $feedback['email'] ?? __('ig-feedback::layouts.email.anonymous') }}
 </p>
-
+@parent
 @endsection
 
 @section('footer')
-<p>
-    @lang('ig-common::messages.email.regards'),<br />
-    {{ \InternetGuru\LaravelCommon\Support\Helpers::getAppInfo() }}<br />
-    {{ url()->previous() }}
-</p>
-@stop
+@parent
+<p>{{ \InternetGuru\LaravelCommon\Support\Helpers::getAppInfo() }}</p>
+@endsection
