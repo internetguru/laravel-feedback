04ca67ee64ff6283da82f66638eaa6b22374f412
George <george@internetguru.io>
2025-11-07 22:09:02 +0100

    Add a form link template

---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,12 @@
+@props([
+    'formId',
+])
+
+<a
+    {{ $attributes }}
+    href="Javascript:void(0)"
+    x-data
+    x-on:click.prevent="Livewire.dispatch('openFeedback', {id: '{{ $formId }}'})"
+>
+    {{ $slot ?? $formId }}
+</a>
