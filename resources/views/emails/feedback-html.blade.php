0cbfdbd1c5beaaf1de4574f0f5a2fa4fafb18a11
George <george@internetguru.io>
2025-11-28 18:33:44 +0100

    Strip tags from headers in email and remove colon

--- resources/views/emails/feedback-html.blade.php
+++ resources/views/emails/feedback-html.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 @foreach($feedback as $field)
 <p>
-    <strong>{{ $field['label'] }}:</strong><br/>
+    <strong>{{ strip_tags($field['label']) }}</strong><br/>
     {!! nl2br(e($field['value'])) !!}
 </p>
 @endforeach
